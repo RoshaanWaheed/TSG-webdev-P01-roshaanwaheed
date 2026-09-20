e145e7b824c282cfb125b934a705398a1c2dedc1
Roshaan Zaib <m.roshaanzaib@gmail.com>
2026-09-20 19:14:27 +0500

    Add Javascript for scroll to top bottom

--- script.js
+++ script.js
@@ -38,3 +38,17 @@ function setActiveLink() {
 }
  
 window.addEventListener('scroll', setActiveLink);
+
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+ 
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 400) {
+    scrollTopBtn.classList.add('visible');
+  } else {
+    scrollTopBtn.classList.remove('visible');
+  }
+});
+ 
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
